f0e40f7a4daf25eb9a3677ea4d05abc9df544e39
Vasu <vasu0123@gmail.com>
2013-11-21 13:28:02 +0530

    Removed unused import

--- JBQRScannerApp/src/com/JBConsmetics/jbqrscannerapp/receivers/NetworkChangeReceiver.java
+++ JBQRScannerApp/src/com/JBConsmetics/jbqrscannerapp/receivers/NetworkChangeReceiver.java
@@ -1,10 +1,10 @@
 package com.JBConsmetics.jbqrscannerapp.receivers;
 
-import com.JBCosmetics.jbqrscannerapp.common.SendRequestForClaims;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import com.JBCosmetics.jbqrscannerapp.common.SendRequestForClaims;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
 
